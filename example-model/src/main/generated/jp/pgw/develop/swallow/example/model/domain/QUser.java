ab5a69c9e1b8648e8ded17519962f1816a06adaa
kazamage <kazamage1980@gmail.com>
2015-06-28 23:34:09 +0900

    add javascript code.

---
+++ example-model/src/main/generated/jp/pgw/develop/swallow/example/model/domain/QUser.java
@@ -0,0 +1,41 @@
+package jp.pgw.develop.swallow.example.model.domain;
+
+import static com.mysema.query.types.PathMetadataFactory.*;
+
+import com.mysema.query.types.path.*;
+
+import com.mysema.query.types.PathMetadata;
+import javax.annotation.Generated;
+import com.mysema.query.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.mysema.query.codegen.EntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 1108944316L;
+
+    public static final QUser user = new QUser("user");
+
+    public final StringPath encodedPassword = createString("encodedPassword");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath username = createString("username");
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata<?> metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
---
+++ src/app/index.run.ts
@@ -0,0 +1,11 @@
+module example {
+  'use strict';
+
+  export class RunBlock {
+    /** @ngInject */
+    constructor($log: ng.ILogService) {
+      $log.debug('runBlock end');
+    }
+
+  }
+}
